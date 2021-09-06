9a1317850e63edd50e2ed2ac37048d0dea4653f3
Leonardo Eichstaedt <leonardoeichs@gmail.com>
2021-09-05 23:48:40 -0300

    first commit

---
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { CarModel } from '../ModelsContext'
+import ModelsContext from '../ModelsContext'
+
+import { Container } from './styles';
+
+
+const ModelsWrapper: React.FC = ({ children }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([])
+
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels(state => [...state, model])
+  }, [])
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels(state => state.filter(model => model.modelName !== modelName))
+  }, [])
+
+  const getModelByName = useCallback((modelName: string) => {
+    return registeredModels.find(item => item.modelName === modelName) || null
+  }, [registeredModels])
+
+  return (
+    <ModelsContext.Provider value={{
+      wrapperRef,
+      registeredModels,
+      registerModel,
+      unregisterModel,
+      getModelByName
+    }}>
+      <Container ref={wrapperRef}>
+        {children}
+      </Container>
+    </ModelsContext.Provider>
+  );
+};
+
+export default ModelsWrapper;
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { ModelsWrapper, ModelSection } from '../Model'
+import DefaultOverlayContent from '../DefaultOverlayContainer'
+
+import { Container } from './styles'
+
+const Page: React.FC = () => {
+    return (
+        <Container>
+            <ModelsWrapper>
+                <div>
+                    {[
+                        'Model One',
+                        'Model Two',
+                        'Model Three',
+                        'Model Four',
+                        'Model Five',
+                        'Model Six',
+                        'Model Seven'
+                    ].map(modelName => (
+                        <ModelSection
+                            key={modelName}
+                            className="colored "
+                            modelName={modelName}
+                            overlayNode={
+                                <DefaultOverlayContent
+                                    label={modelName}
+                                    description="Order Online for Delivery" />
+                            }
+                        />
+                    ))}
+                </div>
+            </ModelsWrapper>
+        </Container>
+    )
+}
+export default Page
